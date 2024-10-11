64f15b78cfb4d2fa102c8045ecab55d31f18e73c
DanielBalubar <152581120+DanielBalubar@users.noreply.github.com>
2024-10-11 02:41:08 +0000

    This is a test doctype

---
+++ my_app01/my_app01_test_balubar/doctype/my_testdoctype/my_testdoctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Balubar Daniel Louis Q and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MyTestDocType(Document):
+	pass
